aba37e7032f5c67a0612d2abc2bfb6e9a29f1f37
yuhongyue <yhy@ebidding.net.cn>
2019-05-22 15:04:35 +0800

    提交代码

---
+++ src/main/java/spring/boot/gradle/HelloGradleController.java
@@ -0,0 +1,12 @@
+package spring.boot.gradle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+    @GetMapping
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
+}
